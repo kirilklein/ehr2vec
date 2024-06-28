749484abc647e82a9d6066c12c7f0a51600a68ea
Kiril Klein <kikl@di.ku.dk>
2024-06-28 16:10:37 +0200

    feat: censor diag at end of visit
    - We can now include diagnoses up to the end of visit

--- ehr2vec/data_fixes/censor.py
+++ ehr2vec/data_fixes/censor.py
@@ -1,4 +1,4 @@
-from typing import List, Union
+from typing import List, Union, Dict
 
 import pandas as pd
 
@@ -11,12 +11,15 @@ logger = logging.getLogger(__name__)
 
 
 class Censorer:
-    def __init__(self, n_hours: int, vocabulary:dict=None) -> None:
+    def __init__(self, n_hours: int, vocabulary:dict=None, censor_diagnoses_at_end_of_visit=False) -> None:
         """Censor the features based on the event timestamp.
         n_hours if positive, censor all items that occur n_hours after event."""
         self.n_hours = n_hours
         self.vocabulary = vocabulary
         self.background_length = None
+        self.censor_diagnoses_at_end_of_visit = censor_diagnoses_at_end_of_visit
+        if self.censor_diagnoses_at_end_of_visit:
+            self.diagnoses_codes = [v for k, v in self.vocabulary if k.startswith('D')]
         
     def __call__(self, features: dict, index_dates: list) -> tuple:
         sample_concepts = features["concept"][0]
@@ -48,28 +51,50 @@ class Censorer:
 
     def _censor_patient(self, patient: dict, index_timestamp: float) -> dict:
         """Censor the patient's features n_hours after index_timestamp (given in abspos)."""
-        if not pd.isna(index_timestamp):
-            # Extract the attention mask and determine the number of non-masked items
-            attention_mask = patient["attention_mask"]
-            num_non_masked = attention_mask.count(1)
-
-            # Extract absolute positions for non-masked items
-            absolute_positions = patient["abspos"][:num_non_masked]
-            
-            # Determine which items to censor based on the event timestamp and background flags
-            censor_flags = self._generate_censor_flags(absolute_positions, index_timestamp)
+        if not pd.isna(index_timestamp):            
+            censor_flags = self._generate_censor_flags(patient, index_timestamp)
             censor_flags[:self.background_length] = [True]*self.background_length # keep background
             for key, value in patient.items():
                 patient[key] = [item for index, item in enumerate(value) if censor_flags[index]]
                 
         return patient
     
-    def _generate_censor_flags(self, absolute_positions: List[float], index_timestamp: float) -> List[bool]:
+    def _generate_censor_flags(self, patient: Dict[str, List], index_timestamp: float) -> List[bool]:
         """Generate flags indicating which items to censor, based on index_timestamp and self.n_hours."""
-        return [
-            position - index_timestamp - self.n_hours <= 0 
-            for position in absolute_positions
-        ]
+        absolute_positions = patient['abspos']
+        censor_flags = [position  <= (index_timestamp + self.n_hours) for position in absolute_positions]
+        if self.censor_diagnoses_at_end_of_visit:
+            censor_flags = self._censor_diagnoses_at_end_of_visit(patient, censor_flags)
+        return censor_flags
+    
+    def _censor_diagnoses_at_end_of_visit(self, patient: Dict[str, list], 
+                                          censor_flags: List[bool]) -> List[bool]:
+        """Include diagnoses up to the end of the visit."""
+        last_segment = self._get_last_segment(censor_flags, patient['segment'])
+        last_index_to_include = self._return_last_index_of_element(patient['segment'], last_segment)
+        diagnoses_flags = self._get_diagnoses_flags(patient['concept'])
+        new_censor_flags = self._combine_flags_with_diagnoses_flags(
+            censor_flags, diagnoses_flags, last_index_to_include) 
+        return new_censor_flags
+    
+    @staticmethod
+    def _combine_flags_with_diagnoses_flags(censor_flags: List[bool], 
+                                            diagnoses_flags: List[bool], 
+                                            last_index_to_include:int) -> List[bool]:
+        return [flag or (diagnoses_flag and (i<=last_index_to_include))    \
+                            for i, (flag, diagnoses_flag) \
+                                in enumerate(zip(censor_flags, diagnoses_flags))]
+
+    def _get_diagnoses_flags(self, concept):
+        return [c in self.diagnoses_codes for c in concept]
+
+    @staticmethod
+    def _get_last_segment(censor_flags: List[bool], segments: List[int])->int:
+        return [seg for seg, flag in zip(segments, censor_flags) if flag][-1]
+    
+    @staticmethod
+    def _return_last_index_of_element(lst: List[bool], element: bool)->int:
+        return len(lst)-1-lst[::-1].index(element)
 
     def _identify_background(self, concepts: List[Union[int, str]], tokenized_flag: bool) -> List[bool]:
         """
@@ -95,6 +120,4 @@ class Censorer:
     def _identify_if_tokenized(concepts:list) -> bool:
         """Identify if the features are tokenized."""
         return concepts and isinstance(concepts[0], int)
-
-
 
--- ehr2vec/tests/test_censor.py
+++ ehr2vec/tests/test_censor.py
@@ -1,5 +1,5 @@
 import unittest
-
+from unittest.mock import patch
 from ehr2vec.data_fixes.censor import Censorer
 
 
@@ -8,6 +8,7 @@ class TestCensorer(unittest.TestCase):
     def setUp(self):
         self.censorer = Censorer(n_hours=1)
         self.censorer.background_length = 3
+
     def test_censor(self):
         features = {
             'concept': [['[CLS]', 'BG_GENDER_Male', '[SEP]', 'Diagnosis1', '[SEP]', 'Diagnosis2', 'Medication1', '[SEP]']],
@@ -38,10 +39,70 @@ class TestCensorer(unittest.TestCase):
 
     def test_generate_censor_flags(self):
         abspos = [0, 0, 0, 1, 1, 2, 3, 3]
+        patient = {'abspos': abspos}
         event_timestamp = 1
-        censor_flags = self.censorer._generate_censor_flags(abspos, event_timestamp)
+        censor_flags = self.censorer._generate_censor_flags(patient, event_timestamp)
 
         self.assertEqual(censor_flags, [True, True, True, True, True, True, False, False])
+
+class TestCensorerDiagnosesAtEndOfVisit(unittest.TestCase):
+
+    def setUp(self):
+        self.censorer = Censorer(n_hours=1)
+        self.censorer.background_length = 3
+        self.censorer.censor_diagnoses_at_end_of_visit = True
+        self.censorer.diagnoses_codes = {'Diagnosis1', 'Diagnosis2'}
+
+        self.features = {
+            'concept': [['[CLS]', 'BG_GENDER_Male', '[SEP]', 'Diagnosis1', 'Diagnosis2', '[SEP]', 'Medication1', 'Medication2','[SEP]']],
+            'abspos': [[0, 0, 0, 1.9, 4, 5, 5.5, 6, 7]],
+            'attention_mask': [[1, 1, 1, 1, 1, 1, 1, 1, 1]],
+            'segment': [[0, 0, 0, 1, 1, 1, 2, 2, 2]]
+        }
+        self.index_dates = [1]
+        self.expected_result = {
+            'concept': [['[CLS]', 'BG_GENDER_Male', '[SEP]', 'Diagnosis1', 'Diagnosis2']],
+            'abspos': [[0, 0, 0, 1.9, 4]],
+            'attention_mask': [[1, 1, 1, 1, 1]],
+            'segment': [[0, 0, 0, 1, 1]]
+        }
+
+        self.patient = {k:v[0] for k,v in self.features.items()}
+        self.expected_flags = [True] * 5 + [False] * 4
+
+    def test_generate_censor_flags(self):
+        censor_flags = self.censorer._generate_censor_flags(self.patient, self.index_dates[0])
+        self.assertEqual(censor_flags, self.expected_flags)
+
+    def test_censor_diagnoses_at_end_of_visit(self):
+        censor_flags = [True] * 4 + [False] * 5
+        new_censor_flags = self.censorer._censor_diagnoses_at_end_of_visit(self.patient, censor_flags)
+        self.assertEqual(new_censor_flags, self.expected_flags)
+
+    def test_get_diagnoses_flags(self):
+        concept = ['Diagnosis1', 'Diagnosis2', 'Medication1', 'Medication2']
+        diagnoses_flags = self.censorer._get_diagnoses_flags(concept)
+        self.assertEqual(diagnoses_flags, [True, True, False, False])
+
+    def test_get_last_segment(self):
+        censor_flags = [True] * 4 + [False] * 3
+        segments = [0, 0, 0, 1, 1, 2, 2]
+        expected_segment = 1
+
+        last_segment = self.censorer._get_last_segment(censor_flags, segments)
+        self.assertEqual(last_segment, expected_segment)
+
+    def test_return_last_index_of_element(self):
+        lst = [0, 0, 1, 1, 1, 2]
+        element = 1
+        expected_index = 4
+
+        last_index = self.censorer._return_last_index_of_element(lst, element)
+        self.assertEqual(last_index, expected_index)
+
+    def test_censor(self):
+        result = self.censorer.censor(self.features, self.index_dates)
+        self.assertEqual(result, self.expected_result)
 
 
 if __name__ == '__main__':
